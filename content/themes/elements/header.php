a0a4871e388201e8723198692b513f7dcacb53b0
Luc Awater <lucawater@gmail.com>
2015-07-08 21:55:14 +0200

    add fonts

--- content/themes/elements/header.php
+++ content/themes/elements/header.php
@@ -28,6 +28,10 @@
   <!-- Stylesheet -->
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/app.css">
 
+  <!-- Fonts form Typekit -->
+  <script src="//use.typekit.net/bux1knv.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+
   <!-- WP_HEAD() -->
   <?php wp_head(); ?>
 
