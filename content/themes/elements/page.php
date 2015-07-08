2c6050e3f80e9b8f14777f7f63252e20cdcee418
Luc Awater <lucawater@gmail.com>
2015-07-08 08:18:50 +0200

    add data and style for days

--- content/themes/elements/page.php
+++ content/themes/elements/page.php
@@ -1,28 +1,43 @@
 <?php
 get_header();
 
-// Loop into ACF groups
-if( have_rows('page') ): $i_anchor = 1; $i_par = 0;
-  while( have_rows('page') ): the_row();
-    
-    // Hero section is placed before main
-    
-    if( get_row_layout() == 'text' ):
-      include( locate_template('content/text.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'image' ):
-      include( locate_template('content/image.php') );
-    elseif( get_row_layout() == 'grid_primary' ):
-      include( locate_template('content/gridPri.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'grid_secondary' ):
-      include( locate_template('content/gridSec.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'slider' ):
-      include( locate_template('content/slider.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'parallax' ): $i_par++;
-      include( locate_template('content/parallax.php') );
-    endif;
-    
-  endwhile;
-endif;
+$mon_name = array( get_field('monday') );
+$mon_items = array( get_field('monday_item') );
+$monday = array_merge( $mon_name, $mon_items );
+
+$tue_name = array( get_field('tuesday') );
+$tue_items = array( get_field('tuesday_item') );
+$tuesday = array_merge( $tue_name, $tue_items );
+
+$wed_name = array( get_field('wednesday') );
+$wed_items = array( get_field('wednesday_item') );
+$wednesday = array_merge( $wed_name, $wed_items );
+
+$thu_name = array( get_field('thursday') );
+$thu_items = array( get_field('thursday_item') );
+$thursday = array_merge( $thu_name, $thu_items );
+
+$fri_name = array( get_field('friday') );
+$fri_items = array( get_field('friday_item') );
+$friday = array_merge( $fri_name, $fri_items );
+
+$days = array( $monday, $tuesday, $wednesday, $thursday, $friday );
+
+// echo '<pre>';
+// print_r($days);
+// echo '</pre>';
+
+foreach( $days as $day ):
+  echo '<div class="day">';
+    echo '<h2>' . $day[0] . '</h2>';
+
+    echo '<ul>';
+      foreach( $day[1] as $single ):
+        echo '<li><p><span>' . $single['item_activity'] . '</span><span>' . get_cat_name( $single['item_project'] ) . '</span><span>' . $single['item_hours'] . '</span></p></li>';
+      endforeach;
+    echo '</ul>';
+  echo '</div>';
+endforeach;
 
 get_footer();
 ?>
